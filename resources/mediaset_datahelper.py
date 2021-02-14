90a0bfbaf8ef2d27adc2a44d33b9ab3e82d1daa3
vincenzo.delloste <vincenzo.delloste@nttdata.com>
2021-02-14 17:55:25 +0100

    fanart and longdescription

--- resources/mediaset_datahelper.py
+++ resources/mediaset_datahelper.py
@@ -1,3 +1,24 @@
+from datetime import datetime, date
+from kodi_six import utils
+try:
+    from functools import reduce
+except:
+    pass
+
+def __reduceTags(x, y):
+    if y['scheme'] in x:
+        x[y['scheme']].append(y['title'])
+    else:
+        x[y['scheme']] = [y['title']]
+    return x
+
+def __normalize(value):
+    if isinstance(value, dict):
+        return dict(map(lambda x: (x[0], __normalize(x[1])), value.items()))
+    elif isinstance(value, list):
+        return map(lambda x: utils.py2_encode(x), value)
+    else:
+        return utils.py2_encode(value)
 
 def _gather_media_type(prog):
     if 'programType' in prog:
@@ -18,10 +39,27 @@ def _gather_media_type(prog):
         return 'movie'
     return 'video'
 
-
-def _gather_info(prog, titlewd=False, mediatype=None, infos=None):
+def _gather_info(prog, titlewd=False, mediatype=None, infos=None, lookup_fullplot=True):
     if infos is None:
         infos = {}
+
+    tags = reduce(__reduceTags , prog['tags'],  {}) if 'tags' in prog else {}
+    if 'genre' not in infos:
+        infos['genre'] = []
+    if 'mediasetprogram$genres' in prog:
+        infos['genre'].extend(prog['mediasetprogram$genres'])
+    if 'mediasettvseason$genres' in prog:
+        infos['genre'].extend(prog['mediasettvseason$genres'])
+    if 'genre' in tags and tags['genre']:
+        infos['genre'].extend(tags['genre'])
+
+    channelCategory = ''
+    if 'mediasetprogram$channelCategory' in prog and prog['mediasetprogram$channelCategory']:
+        channelCategory = prog['mediasetprogram$channelCategory'][0]
+    elif 'mediasettvseason$channelCategory' in prog and prog['mediasettvseason$channelCategory']:
+        channelCategory = prog['mediasettvseason$channelCategory'][0]
+    elif 'channelCategory' in tags and tags['channelCategory']:
+        channelCategory = tags['channelCategory'][0]
 
     if 'mediatype' not in infos:
         if mediatype:
@@ -34,8 +72,7 @@ def _gather_info(prog, titlewd=False, mediatype=None, infos=None):
             infos['title'] = prog["title"]
         elif 'mediasetprogram$brandTitle' in prog:
             infos['title'] = prog["mediasetprogram$brandTitle"]
-        if (titlewd and 'mediasetprogram$brandTitle' in prog and
-                prog['mediasetprogram$brandTitle'] != ''):
+        if (titlewd and 'mediasetprogram$brandTitle' in prog and prog['mediasetprogram$brandTitle'] != ''):
             infos['title'] = '{} - {}'.format(prog['mediasetprogram$brandTitle'], infos['title'])
 
     if 'credits' in prog:
@@ -53,26 +90,31 @@ def _gather_info(prog, titlewd=False, mediatype=None, infos=None):
         plot = ""
         plotoutline = ""
         # try to find plotoutline
-        if 'shortDescription' in prog:
+        if 'shortDescription' in prog and prog['shortDescription']:
             plotoutline = prog["shortDescription"]
-        elif 'mediasettvseason$shortDescription' in prog:
+        elif 'mediasettvseason$shortDescription' in prog and channelCategory != 'MediasetPlay_Programmi Tv':
             plotoutline = prog["mediasettvseason$shortDescription"]
-        elif 'description' in prog:
+        elif 'description' in prog and prog['description']:
             plotoutline = prog["description"]
+        elif 'mediasettvseason$brandDescription' in prog:
+            plotoutline = prog["mediasettvseason$brandDescription"]
         elif 'mediasetprogram$brandDescription' in prog:
             plotoutline = prog["mediasetprogram$brandDescription"]
         elif 'mediasetprogram$subBrandDescription' in prog:
             plotoutline = prog["mediasetprogram$subBrandDescription"]
 
         # try to find plot
-        # if 'longDescription' in prog: # longDescription sometimes has -
-        #    plot = prog["longDescription"]
-        if 'description' in prog:
-            plotoutline = prog["description"]
-        elif 'mediasetprogram$brandDescription' in prog:
-            plot = prog["mediasetprogram$brandDescription"]
-        elif 'mediasetprogram$subBrandDescription' in prog:
-            plot = prog["mediasetprogram$subBrandDescription"]
+        if lookup_fullplot:
+            if 'longDescription' in prog and prog['longDescription'] and channelCategory != 'MediasetPlay_Programmi Tv': # longDescription sometimes has -
+                plot = prog["longDescription"]
+            elif 'description' in prog and prog['description']:
+                plot = prog["description"]
+            elif 'mediasettvseason$brandDescription' in prog:
+                plot = prog["mediasettvseason$brandDescription"]
+            elif 'mediasetprogram$brandDescription' in prog:
+                plot = prog["mediasetprogram$brandDescription"]
+            elif 'mediasetprogram$subBrandDescription' in prog:
+                plot = prog["mediasetprogram$subBrandDescription"]
 
         # fill the other if one is empty
         if plot == "":
@@ -82,37 +124,48 @@ def _gather_info(prog, titlewd=False, mediatype=None, infos=None):
         infos['plot'] = plot
         infos['plotoutline'] = plotoutline
 
-    if 'genre' not in infos:
-        infos['genre'] = []
-    if 'mediasetprogram$genres' in prog:
-        infos['genre'].extend(prog['mediasetprogram$genres'])
-    if 'mediasettvseason$genres' in prog:
-        infos['genre'].extend(prog['mediasettvseason$genres'])
-    if 'tags' in prog and prog['tags']:
-        for t in prog['tags']:
-            if t['scheme'] == 'genre':
-                infos['genre'].append(t['title'])
+    lastPublished = 0
+    if 'mediasetprogram$publishInfo_lastPublished' in prog:
+        try:
+            lastPublished = int(prog['mediasetprogram$publishInfo_lastPublished'])/1000
+        except:
+            pass
+    if 'aired' not in infos and lastPublished:
+        try:
+            infos['aired'] = str(date.fromtimestamp(lastPublished))
+        except:
+            pass
+    if 'dateadded' not in infos and lastPublished:
+        try:
+            infos['dateadded'] = str(datetime.fromtimestamp(lastPublished))
+        except:
+            pass
 
     if 'duration' not in infos and 'mediasetprogram$duration' in prog:
         infos['duration'] = prog['mediasetprogram$duration']
     if 'year' not in infos and 'year' in prog:
         infos['year'] = prog['year']
-    if 'season' not in infos and 'tvSeasonNumber' in prog:
+    if 'season' not in infos and 'tvSeasonNumber' in prog and prog['tvSeasonNumber']:
         infos['season'] = prog['tvSeasonNumber']
-    if 'episode' not in infos and 'tvSeasonEpisodeNumber' in prog:
-        infos['episode'] = prog['tvSeasonEpisodeNumber']
-
+    if 'episode' not in infos:
+        if 'tvSeasonEpisodeNumber' in prog and prog['tvSeasonEpisodeNumber']:
+            infos['episode'] = prog['tvSeasonEpisodeNumber']
+        elif 'season' in infos:
+            del infos['season']
     if ('season' in infos and 'episode' in infos):
         infos['tvshowtitle'] = prog['mediasetprogram$brandTitle']
 
     if 'program' in prog:
         return _gather_info(prog['program'], titlewd=titlewd, infos=infos)
-    return infos
+
+    return __normalize(infos)
 
 
 def _gather_art(prog):
     arts = {}
     if 'thumbnails' in prog:
+
+        # Thumb
         if 'image_vertical-264x396' in prog['thumbnails']:
             arts['poster'] = prog['thumbnails']['image_vertical-264x396']['url']
             arts['thumb'] = arts['poster']
@@ -120,18 +173,41 @@ def _gather_art(prog):
             arts['poster'] = prog['thumbnails']['channel_logo-100x100']['url']
             arts['thumb'] = arts['poster']
 
+        # Banner
         if 'brand_cover-1440x513' in prog['thumbnails']:
             arts['banner'] = prog['thumbnails']['brand_cover-1440x513']['url']
         elif 'image_header_poster-1440x630' in prog['thumbnails']:
             arts['banner'] = prog['thumbnails']['image_header_poster-1440x630']['url']
         elif 'image_header_poster-1440x433' in prog['thumbnails']:
             arts['banner'] = prog['thumbnails']['image_header_poster-1440x433']['url']
+
+        # Landscape
         if 'image_header_poster-1440x630' in prog['thumbnails']:
             arts['landscape'] = prog['thumbnails']['image_header_poster-1440x630']['url']
         elif 'image_header_poster-1440x433' in prog['thumbnails']:
             arts['landscape'] = prog['thumbnails']['image_header_poster-1440x433']['url']
+
+        # Icon
         if 'brand_logo-210x210' in prog['thumbnails']:
             arts['icon'] = prog['thumbnails']['brand_logo-210x210']['url']
+
+        # Fanart
+        if 'image_horizontal_cover-704x396' in prog['thumbnails']:
+            arts['fanart'] = prog['thumbnails']['image_horizontal_cover-704x396']['url']
+        elif 'image_header_poster-1440x630' in prog['thumbnails']:
+            arts['fanart'] = prog['thumbnails']['image_header_poster-1440x630']['url']
+        elif 'image_header_poster-768x384' in prog['thumbnails']:
+            arts['fanart'] = prog['thumbnails']['image_header_poster-768x384']['url']
+
+        # Cleart
+        if 'image_keyframe_poster-1200x630' in prog['thumbnails']:
+            arts['clearart'] = prog['thumbnails']['image_keyframe_poster-1200x630']['url']
+        
+        # if 'programType' in prog and (prog['programType'] == 'extra' or prog['programType'] == 'episode'):
+        #     if 'image_keyframe_poster-652x367' in prog['thumbnails']:
+        #         arts['poster'] = prog['thumbnails']['image_keyframe_poster-652x367']['url']
+        #         arts['thumb'] = prog['thumbnails']['image_keyframe_poster-652x367']['url']
+
     elif 'program' in prog:
         return _gather_art(prog['program'])
-    return arts
+    return __normalize(arts)
