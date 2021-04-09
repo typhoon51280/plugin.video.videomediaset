4c86605a490ccfc0ed58e1ae933d2685aaf7327b
vincenzo.delloste <vincenzo.delloste@nttdata.com>
2021-04-10 00:54:08 +0200

    fix title tvseries

--- resources/mediaset_datahelper.py
+++ resources/mediaset_datahelper.py
@@ -68,7 +68,9 @@ def _gather_info(prog, titlewd=False, mediatype=None, infos=None, lookup_fullplo
             infos['mediatype'] = _gather_media_type(prog)
 
     if 'title' not in infos:
-        if 'title' in prog:
+        if 'mediasetprogram$subBrandId' in prog and 'description' in prog and prog["description"] and 'media' not in prog:
+            infos['title'] = prog["description"]
+        elif 'title' in prog:
             infos['title'] = prog["title"]
         elif 'mediasetprogram$brandTitle' in prog:
             infos['title'] = prog["mediasetprogram$brandTitle"]
