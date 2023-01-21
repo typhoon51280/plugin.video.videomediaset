045fe903a91ddfb87f6e06aabc9d12f7843ece48
typhoon51280 <typhoon51280@users.noreply.github.com>
2023-01-21 11:54:24 +0100

    fix User Agent to latest Chrome
    Signed-off-by: typhoon51280 <typhoon51280@users.noreply.github.com>

--- resources/lib/monitor.py
+++ resources/lib/monitor.py
@@ -25,7 +25,6 @@ class MediasetService:
 
     def __init__(self):
         self.med = Mediaset()
-        self.med.setUserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
         self.med.log = kodiutils.log
         self.scrobblingInterval = kodiutils.getSettingAsNum("scrobblingInterval")
         kodiutils.log('[mediasetservice] monitorService init')
